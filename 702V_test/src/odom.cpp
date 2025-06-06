9e1881d2f8598a0ddc344d62386a58bea7f67139
neonmechanics <team702v@gmail.com>
2025-06-06 12:46:58 -0700

    non working commit, fix given error "cannot overload functions distinguished by return type alone"

--- 702V_test/src/odom.cpp
+++ 702V_test/src/odom.cpp
@@ -1,7 +1,7 @@
 #include "odom.h"
 #include "vex.h"
 #include "types.h"
-Pose getpos(void){
+Pose getpos(void) {
 float leftpos = left_drive.position(degrees)/ 360.0 * M_PI * 3.25;
 float rightpos = right_drive.position(degrees)/360.0 * M_PI * 3.25;
 float pos = prevpos - (leftpos + rightpos) / 2.0;
